f0874d77da62c57b2fa14a21b4678b2b6433d396
taehwan1006 <105338569+taehwan1006@users.noreply.github.com>
2022-09-22 14:11:14 +0900

    Add files via upload

---
+++ DoubleTester.java
@@ -0,0 +1,8 @@
+
+public class DoubleTester {
+	public static void main(String args[]) {
+		double probability = 8.70;
+		int percentage = (int) (10 * Math.round(probability * 10));
+		System.out.println(percentage);
+	}
+}
